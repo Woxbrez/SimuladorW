b21d3fe5434084299914b77e557083bb74ec65ab
Daniel Soare <danielsoaresf@hotmail.com>
2018-06-26 05:28:45 -0300

    Configurado Maven, libs ok

---
+++ src/main/java/br/com/treinando/App.java
@@ -0,0 +1,13 @@
+package br.com.treinando;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
